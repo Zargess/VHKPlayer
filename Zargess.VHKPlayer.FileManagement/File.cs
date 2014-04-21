df9782dd944597e70501a389c182424bb02e0cd7
Zargess-mobile <costa@hotmail.dk>
2014-04-21 11:17:52 +0200

    Initial commit

---
+++ Zargess.VHKPlayer.FileManagement/File.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using Zargess.VHKPlayer.Enums;
+
+namespace Zargess.VHKPlayer.FileManagement {
+    public class File {
+        private string _fullpath;
+        public string Name { get; private set; }
+        public string Source { get; private set; }
+        public string Extension { get; private set; }
+        public string NameWithNoExtension { get; private set; }
+        public FileType Type { get; private set; }
+        public string FullPath {
+            get {
+                return _fullpath;
+            }
+            set {
+                _fullpath = value;
+                var temp = PathHandler.SplitPath(_fullpath);
+                Name = temp[temp.Length - 1];
+                if (temp.Length > 1) {
+                    Source = temp[temp.Length - 2];
+                }
+                var extens = _fullpath.Split('.');
+                Extension = extens[extens.Length - 1];
+                var noextens = Name.Split('.');
+                for (var i = 0; i < noextens.Count() - 1; i++) {
+                    NameWithNoExtension += noextens[i];
+                }
+                SetFileType(Extension);
+            }
+        }
+
+        private void SetFileType(string e) {
+            switch (e) {
+                case "jpg":
+                case "png":
+                    Type = FileType.Picture;
+                    break;
+                case "avi":
+                    Type = FileType.Video;
+                    break;
+                case "mp3":
+                    Type = FileType.Music;
+                    break;
+                default:
+                    Type = FileType.Unsupported;
+                    break;
+            }
+        }
+
+        public File(string path) {
+            FullPath = path;
+        }
+    }
+}
---
+++ Zargess.VHKPlayer.FileManagement/PathHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zargess.VHKPlayer.FileManagement {
+    public class PathHandler {
+        public static string[] SplitPath(string path) {
+            var res = new string[0];
+            if (path.Contains(@"\")) {
+                const string splitcode = @"\";
+                var splitchar = splitcode.ToCharArray()[0];
+                res = path.Split(splitchar);
+            } else if (path.Contains("/")) {
+                res = path.Split('/');
+            }
+            return res;
+        }
+
+        public static string CombinePaths(string path1, string path2) {
+            return Path.Combine(path1, path2);
+        }
+    }
+}
