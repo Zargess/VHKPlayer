9468062fc0fc218f074fc2baef4e859f053a373a
Zargess-mobile <costa@hotmail.dk>
2014-04-22 17:26:39 +0200

    Begun to create memory loading algo

---
+++ Zargess.VHKPlayer.FileManagement/MemoryManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zargess.VHKPlayer.FileManagement {
+    public class MemoryManager {
+
+        public MemoryManager(string path) {
+            
+        }
+    }
+}
