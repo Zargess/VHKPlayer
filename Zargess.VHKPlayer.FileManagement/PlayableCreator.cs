abe9cb287328a31cb3eba8c8a3d2ed18d04838c9
Zargess-mobile <costa@hotmail.dk>
2014-06-15 22:51:45 +0200

    Started on making the loading system

---
+++ Zargess.VHKPlayer.FileManagement/PlayableCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zargess.VHKPlayer.FileManagement {
+    public class PlayableCreator {
+        public List<IPlayable> CreatePlayables(string type, string[] parameters) {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ Zargess.VHKPlayer.FileManagement/Playlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zargess.VHKPlayer.Enums;
+
+namespace Zargess.VHKPlayer.FileManagement {
+    public class Playlist : IPlayable {
+        public List<FileNode> Music { get; protected set; }
+        public List<FileNode> Video { get; protected set; }
+        public List<Procedure> Procedures { get; protected set; }
+        public bool Repeatable { get; protected set; }
+
+
+    }
+}
