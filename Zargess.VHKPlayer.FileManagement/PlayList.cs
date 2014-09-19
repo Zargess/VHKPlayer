fc86cc6becb4ee98ca0d00adf3d35803d7121970
Zargess-mobile <costa@hotmail.dk>
2014-09-19 22:30:55 +0200

    Started on making playlists

---
+++ Zargess.VHKPlayer.FileManagement/PlayList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zargess.VHKPlayer.FileManagement {
+    public class PlayList {
+        public List<FileNode> Content { get; private set; }
+        public string Name { get; set; }
+
+        public PlayList(string name) {
+            Name = name;
+            Content = new List<FileNode>();
+        }
+
+        public PlayList(string name, List<FileNode> content) : this(name) {
+            AddRange(content);
+        }
+
+        public void AddRange(List<FileNode> list) {
+            list.ForEach(x => Content.Add(new FileNode(x.FullPath)));
+        }
+
+        public void Add(FileNode file) {
+            Content.Add(new FileNode(file.FullPath));
+        }
+
+        public FileNode GetFile(int index) {
+            return Content[index];
+        }
+    }
+}
---
+++ Zargess.VHKPlayer.FileManagement/SpecialList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zargess.VHKPlayer.FileManagement {
+    class SpecialList : PlayList {
+        private int Counter { get; set; }
+
+        public SpecialList(string name) : base(name) {
+            Counter = 0;
+        }
+
+        public SpecialList(string name, List<FileNode> content) : base(name, content) {
+            Counter = 0;
+        }
+
+        public FileNode GetNext() {
+            var file = Content[Counter];
+            Counter = Counter == Content.Count ? 0 : Counter++;
+
+            return file;
+        }
+
+        public FileNode GetCurrent() {
+            if (Counter == Content.Count) {
+                Counter = 0;
+            }
+            return Content[Counter];
+        }
+    }
+}
