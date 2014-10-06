38cb60d3209403d0bf2d2fce958d88d46cfa8282
Zargess <haagh9@hotmail.com>
2014-10-06 07:05:40 +0200

    Multithreaded loading done(?)

--- Zargess.VHKPlayer.GUI/MainViewModel.cs
+++ Zargess.VHKPlayer.GUI/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -14,12 +15,43 @@ using Zargess.VHKPlayer.LoadingPolicies;
 using Zargess.VHKPlayer.Settings;
 
 namespace Zargess.VHKPlayer.GUI {
-    public class MainViewModel {
+    public class MainViewModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
         public ObservableSafeCollection<Player> Players { get; private set; }
         public ObservableSafeCollection<Player> People { get; private set; }
         public ObservableSafeCollection<FolderNode> Audio { get; private set; }
         public ObservableSafeCollection<FolderNode> Video { get; private set; }
         public ObservableSafeCollection<PlayList> PlayLists { get; private set; }
+        private bool _foldershowable;
+        private bool _playlistshowable;
+        private bool _peopleshowable;
+        public bool FolderShowable {
+            get { return _foldershowable; }
+            private set {
+                _foldershowable = value;
+                if (PropertyChanged != null) {
+                    PropertyChanged(this,new PropertyChangedEventArgs("_foldershowable"));
+                }
+            }
+        }
+        public bool PlayListShowable {
+            get { return _playlistshowable; }
+            set {
+                _playlistshowable = value;
+                if (PropertyChanged != null) {
+                    PropertyChanged(this, new PropertyChangedEventArgs("_playlistshowable"));
+                }
+            }
+        }
+        public bool PeopleShowable {
+            get { return _playlistshowable; }
+            set {
+                _peopleshowable = value;
+                if (PropertyChanged != null) {
+                    PropertyChanged(this, new PropertyChangedEventArgs("_peopleshowable"));
+                }
+            }
+        }
 
         public MainViewModel() {
             Players = new ObservableSafeCollection<Player>();
@@ -27,6 +59,9 @@ namespace Zargess.VHKPlayer.GUI {
             Audio = new ObservableSafeCollection<FolderNode>();
             Video = new ObservableSafeCollection<FolderNode>();
             PlayLists = new ObservableSafeCollection<PlayList>();
+            FolderShowable = false;
+            PlayListShowable = false;
+            PeopleShowable = false;
         }
 
         public void LoadStructure(string path) {
@@ -52,12 +87,9 @@ namespace Zargess.VHKPlayer.GUI {
                 var root = new FolderNode(path);
                 if (root.ValidRootFolder()) {
                     ClearData();
-                    var folderthread = new Thread(() => LoadFolders(root));
-                    var playlistthread = new Thread(() => LoadPlayLists(root));
-                    var playersthread = new Thread(() => LoadPlayers(root));
-                    folderthread.Start();
-                    playersthread.Start();
-                    playlistthread.Start();
+                    new Thread(() => LoadFolders(root)).Start();
+                    new Thread(() => LoadPlayLists(root)).Start();
+                    new Thread(() => LoadPlayers(root)).Start();
                 } else {
                     Console.WriteLine("Could not load: {0}\nPlease choose another folder.", path);
                 }
@@ -69,6 +101,12 @@ namespace Zargess.VHKPlayer.GUI {
         }
 
         public void LoadFolders(FolderNode root) {
+            if (!root.ValidRootFolder()) {
+                return;
+            }
+            FolderShowable = false;
+            ClearFolderList(Video);
+            ClearFolderList(Audio);
             var limits = ((string)SettingsManager.GetSetting("limits")).Split(',').ToList();
 
             var folders = FolderLoading.getSomeFolders(root.FullPath, Utils.ToFSharpList(limits)).Select(x => new FolderNode(x)).ToList();
@@ -82,6 +120,7 @@ namespace Zargess.VHKPlayer.GUI {
                     folder.InitWatcher();
                 }
             }
+            FolderShowable = true;
             Console.WriteLine("Folder done loading");
         }
 
@@ -90,9 +129,10 @@ namespace Zargess.VHKPlayer.GUI {
         }
 
         public void LoadPlayLists(FolderNode root) {
-            PlayLists.Clear();
             var reks = PathHandler.CombinePaths(root.FullPath, "rek");
             if (Directory.Exists(reks)) {
+                PlayListShowable = false;
+                PlayLists.Clear();
                 PlayLists.Add(new PlayList(PlaylistLoading.sortedPlaylist(reks, "RekFørKamp", 1)));
                 PlayLists.Add(new PlayList(PlaylistLoading.sortedPlaylist(reks, "RekHalvej1", 2)));
                 PlayLists.Add(new PlayList(PlaylistLoading.sortedPlaylist(reks, "RekHalvej2", 3)));
@@ -110,13 +150,14 @@ namespace Zargess.VHKPlayer.GUI {
             } else {
                 Console.WriteLine("The folder {0} does not exists. Please select a proper root folder", reks);
             }
-
+            PlayListShowable = true;
             Console.WriteLine("PlayList done loading");
         }
 
         // TODO : Consider making a Person class and make the Player class a subclass of Person. Then a trainer won't be a Player.
         public void LoadPlayers(FolderNode root) {
             if (!root.ContainsFolder("Spiller") || !root.ContainsFolder("SpillerVideo") || !root.ContainsFolder("SpillerVideoStat")) return;
+            PeopleShowable = false;
             var people = PlayerLoading.createAllPlayers(root.FullPath).ToList();
             ClearPeople(People);
             ClearPeople(Players);
@@ -129,7 +170,7 @@ namespace Zargess.VHKPlayer.GUI {
             }
 
             //Players.ToList().ForEach(Console.WriteLine);
-
+            PeopleShowable = true;
             Console.WriteLine("Players done loading");
         }
 
