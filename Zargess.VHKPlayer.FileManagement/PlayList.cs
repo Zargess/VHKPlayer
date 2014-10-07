b509d2b0ac5ca1d5c9d9268ad3e570136d253ee7
Zargess-mobile <costa@hotmail.dk>
2014-10-07 15:28:56 +0200

    Implementing initwatcher in playlist

--- Zargess.VHKPlayer.FileManagement/PlayList.cs
+++ Zargess.VHKPlayer.FileManagement/PlayList.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Zargess.VHKPlayer.LoadingPolicies;
 
 namespace Zargess.VHKPlayer.FileManagement {
-    public abstract class PlayList {
+    public abstract class PlayList : IWatchable {
         // TODO : Consider implementing a filesystemwatcher for changes. Consider making this an abstract class and then implement a watcher in each sub type.
         // TODO : Rethink the entire concept of playlists. How to get the next element in the list? When do we stop playing this playlist? Should this be controlled in a videoqueue object?
         public List<FileNode> Content { get; private set; }
         public string Name { get; private set; }
+        public FileSystemWatcher Watcher { get; protected set; }
 
         protected PlayList(string name) {
             Name = name;
             Content = new List<FileNode>();
+            InitWatcher();
         }
 
         protected PlayList(string name, List<FileNode> content) : this(name) {
@@ -26,7 +29,7 @@ namespace Zargess.VHKPlayer.FileManagement {
         }
 
         public void AddRange(List<FileNode> list) {
-            list.ForEach(x => Content.Add(new FileNode(x.FullPath)));
+            list.ForEach(x => Add(new FileNode(x.FullPath)));
         }
 
         public void Add(FileNode file) {
@@ -35,6 +38,15 @@ namespace Zargess.VHKPlayer.FileManagement {
 
         public FileNode GetFile(int index) {
             return Content[index];
+        }
+
+        public abstract void InitWatcher();
+
+        public void StopListening() {
+            if (Watcher == null) return;
+            Watcher.EnableRaisingEvents = false;
+            Watcher.Dispose();
+            Watcher = null;
         }
     }
 }
--- Zargess.VHKPlayer.FileManagement/SortedPlayList.cs
+++ Zargess.VHKPlayer.FileManagement/SortedPlayList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,13 +8,23 @@ using Zargess.VHKPlayer.LoadingPolicies;
 
 namespace Zargess.VHKPlayer.FileManagement {
     public class SortedPlayList : PlayList {
+        public int Index { get; private set; }
+
+
         public SortedPlayList(string name, int index) : base(name) {
+            Index = index;
         }
 
-        public SortedPlayList(string name, List<FileNode> content, int index) : base(name, content) {
+        public SortedPlayList(string name, List<FileNode> content, int index) : base(name, content){
+            Index = index;
         }
 
         public SortedPlayList(PlaylistLoading.Playlist list, int index) : base(list) {
+            Index = index;
+        }
+
+        public override void InitWatcher() {
+            throw new NotImplementedException();
         }
     }
 }
