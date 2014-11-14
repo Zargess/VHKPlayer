56e0c9640dcc6c151eeeb67a0cef15ab21966801
Zargess-mobile <costa@hotmail.dk>
2014-11-14 21:15:51 +0100

    Selection strategy

---
+++ New Testable version/VHKPlayer/Zargess.VHKPlayer.FileManagement/SelectAllFilesStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zargess.VHKPlayer.FileManagement.Test {
+    public class AllFileSelectionStrategy : IFileSelectionStrategy {
+        public Queue<IFile> SelectFiles(IPlayable playable) {
+            var res = new Queue<IFile>();
+            foreach (var file in playable.Content) {
+                res.Enqueue(new FileNode(file.FullPath));
+            }
+            return res;
+        }
+    }
+}
