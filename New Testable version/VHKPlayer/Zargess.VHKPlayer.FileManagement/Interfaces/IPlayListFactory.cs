3797d009442e041edff59c0f332af69714bf487c
Zargess <haagh9@hotmail.com>
2014-11-18 23:22:31 +0100

    Prepared for Factory pattern

---
+++ New Testable version/VHKPlayer/Zargess.VHKPlayer.FileManagement/Interfaces/IPlayListFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zargess.VHKPlayer.FileManagement.Factories {
+    public interface IPlayListFactory {
+        string CreateName();
+        IFolder CreateFolder();
+        IFileSelectionStrategy CreateSelectionStrategy();
+        ILoadingStrategy CreateLoadingStrategy();
+    }
+}
