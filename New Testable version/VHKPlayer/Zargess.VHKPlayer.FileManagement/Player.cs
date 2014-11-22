d2f45a4090525f376f252bd3e6ce30717331a91f
Zargess-mobile <costa@hotmail.dk>
2014-11-22 19:10:04 +0100

    Started work on Player

---
+++ New Testable version/VHKPlayer/Zargess.VHKPlayer.FileManagement/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zargess.VHKPlayer.FileManagement {
+    public class Player : IPlayable {
+        public ILoadingStrategy LoadingStrategy { get; private set; }
+
+        public string Name { get; private set; }
+
+        public int Size { get; private set; }
+
+        public ObservableCollection<IFile> GetContent() {
+            throw new NotImplementedException();
+        }
+
+        public Queue<IFile> Play(PlayType pt) {
+            throw new NotImplementedException();
+        }
+    }
+}
