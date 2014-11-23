8f37ff9762a40ac0c5b3840642f69c5ae8c3a8c7
Zargess <haagh9@hotmail.com>
2014-11-23 23:12:45 +0100

    Some stuff

---
+++ New Testable version/VHKPlayer/Zargess.VHKPlayer.FileManagement/Strategies/Selection/IPlayer/VideoSelectionStrategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zargess.VHKPlayer.FileManagement.Interfaces;
+using Zargess.VHKPlayer.SettingsManager;
+using Zargess.VHKPlayer.UtilFunctions;
+
+namespace Zargess.VHKPlayer.FileManagement.Strategies.Selection.IPlayer {
+    public class VideoSelectionStrategy : IFileSelectionStrategy {
+
+        public IFileSelectionStrategy PicSelection { get; private set; }
+
+        public VideoSelectionStrategy(IFileSelectionStrategy picSelection) {
+            PicSelection = picSelection;
+        }
+
+        public Queue<IFile> SelectFiles(IPlayable playable) {
+            var res = new Queue<IFile>();
+            var temp = SettingsManagement.GetStringSetting("playerFolders").Split(';');
+            var vidFolderPath = PathHandler.AbsolutePath(temp[1]);
+            if (!playable.GetContent().Any(x => x.FullPath.ToLower().Contains(vidFolderPath))) return PicSelection.SelectFiles(playable);
+            res.Enqueue(playable.GetContent().Single(x => x.FullPath.ToLower().Contains(vidFolderPath)));
+            return res;
+        }
+    }
+}
