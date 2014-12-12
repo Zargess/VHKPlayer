cfedbd718a4785e78e28e74ee949368afc593440
Zargess <haagh9@hotmail.com>
2014-12-13 00:22:34 +0100

    IPlayManager

---
+++ Restructured/VHKPlayer/Zargess.VHKPlayer/Interfaces/IPlayManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zargess.VHKPlayer.Enums;
+
+namespace Zargess.VHKPlayer.Interfaces {
+    public interface IPlayManager {
+        Queue<IPlayable> Queue { get; }
+
+        void PlayQueue();
+        void Play(IPlayable playable, PlayType type);
+    }
+}
