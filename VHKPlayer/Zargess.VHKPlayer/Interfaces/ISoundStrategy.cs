534d27b344a5675e7bbec8e6e8c5b481f2980f9d
Marcus Haagh <haagh9@hotmail.com>
2014-12-25 22:53:46 +0100

    Started on sound design and made a todo

---
+++ VHKPlayer/Zargess.VHKPlayer/Interfaces/ISoundStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zargess.VHKPlayer.Interfaces {
+    public interface ISoundStrategy {
+        // TODO : Think of a good design for this strategy pattern. It needs to change between select the current sound level or fade in and out the sound. Also when faded option is chosen make a thread that manages when the sound should fade out and then apply it.
+        void Starting();
+        void Stoping();
+    }
+}
