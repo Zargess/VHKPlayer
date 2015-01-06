8d49e5ec58c505fe702c93859a251ec689bd853d
Zargess <haagh9@hotmail.com>
2015-01-06 19:04:47 +0100

    Started over

---
+++ VHKPlayer/VHKPlayer.Test/Tests/FileTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VHKPlayer.Test.Utility;
+using VHKPlayer.Interfaces;
+using VHKPlayer.Model;
+using VHKPlayer.Enums;
+
+namespace VHKPlayer.Test.Tests {
+    /// <summary>
+    /// Summary description for FileTest
+    /// </summary>
+    [TestClass]
+    public class FileTest {
+        [TestInitialize]
+        public void Setup() {
+            Constants.GetRootFolder();
+        }
+
+        [TestMethod]
+        public void Test_txtNameShouldBeTest_txt() {
+            IFile file = new FileNode("Test.txt");
+            Assert.AreEqual("Test.txt", file.Name);
+        }
+
+        [TestMethod]
+        public void PathToTest_txtNameShouldBeTest_txt() {
+            IFile file = new FileNode(Environment.CurrentDirectory + @"\Test.txt");
+            Assert.AreEqual("Test.txt", file.Name);
+        }
+
+        [TestMethod]
+        public void Test_txtPathShouldBePath_Test_txt() {
+            IFile file = new FileNode("Test.txt");
+            Assert.AreEqual(Environment.CurrentDirectory + @"\Test.txt", file.FullPath);
+        }
+
+        [TestMethod]
+        public void Logo_pngPathShouldBePath_Logo_png() {
+            IFile file = new FileNode("Logo.png");
+            Assert.AreEqual(Environment.CurrentDirectory + @"\Logo.png", file.FullPath);
+        }
+
+        [TestMethod]
+        public void PathToLogo_pngPathShouldBePath_Logo_png() {
+            IFile file = new FileNode(Environment.CurrentDirectory + @"\Logo.png");
+            Assert.AreEqual(Environment.CurrentDirectory + @"\Logo.png", file.FullPath);
+        }
+
+        [TestMethod]
+        public void SourceOfLogo_pngPathShouldBeFiles_for_unit_test() {
+            IFile file = new FileNode(Environment.CurrentDirectory + @"\Logo.png");
+            Assert.AreEqual("Files for unit test", file.Source);
+        }
+
+        [TestMethod]
+        public void Logo_pngShouldExist() {
+            IFile file = new FileNode(Environment.CurrentDirectory + @"\Logo.png");
+            Assert.IsTrue(file.Exists());
+        }
+
+        [TestMethod]
+        public void Temp_pngShouldNotExist() {
+            IFile file = new FileNode(Environment.CurrentDirectory + @"\Temp.png");
+            Assert.IsFalse(file.Exists());
+        }
+
+        [TestMethod]
+        public void TypeOfLogo_pngShouldBePicture() {
+            IFile file = new FileNode(Environment.CurrentDirectory + @"\Logo.png");
+            Assert.AreEqual(FileType.Picture, file.Type);
+        }
+
+        [TestMethod]
+        public void TypeOfTest_txtShouldBeUnsupported() {
+            IFile file = new FileNode(Environment.CurrentDirectory + @"\Test.txt");
+            Assert.AreEqual(FileType.Unsupported, file.Type);
+        }
+    }
+}
---
+++ VHKPlayer/VHKPlayer.Test/Utility/Constants.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VHKPlayer.Test.Utility {
+    public class Constants {
+        public static string GetRootFolder() {
+            var path = @"D:\Github";
+            var path2 = @"C:\users\mfh\vhk";
+            var res = "";
+            var statfolder = "";
+            var currentfolder = "";
+            if (Directory.Exists(path)) {
+                res = @"D:\Dropbox\Programmering\C#\damer 2013-2014";
+                statfolder = @"D:\Dropbox\Programmering\C#\digimatch";
+                currentfolder = @"D:\GitHub\VHKPlayer\Files for unit test";
+            } else if (Directory.Exists(path2)) {
+                res = @"c:\users\mfh\dropbox\programmering\c#\damer 2013-2014";
+                statfolder = @"c:\users\mfh\dropbox\programmering\c#\digimatch";
+                currentfolder = @"C:\Users\MFH\Documents\GitHub\VHKPlayer\Files for unit test";
+            } else {
+                res = @"C:\Users\Marcus\Dropbox\Programmering\C#\damer 2013-2014";
+                statfolder = @"c:\users\marcus\Dropbox\Programmering\C#\digimatch";
+                currentfolder = @"C:\Users\Marcus\Documents\GitHub\VHKPlayer\Files for unit test";
+            }
+            //App.ConfigService.Update("statsFolder", statfolder);
+            Environment.CurrentDirectory = currentfolder;
+            return res;
+        }
+    }
+}
