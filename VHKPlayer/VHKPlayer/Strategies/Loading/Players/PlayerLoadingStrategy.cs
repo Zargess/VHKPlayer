2ea7b75ec8adf2fff9732105b2964db1fb90319f
Marcus Haagh <haagh9@hotmail.com>
2015-01-10 20:39:57 +0100

    Started making a LoadingStrategy

---
+++ VHKPlayer/VHKPlayer/Strategies/Loading/Players/PlayerLoadingStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VHKPlayer.Interfaces;
+
+namespace VHKPlayer.Strategies.Loading.Players {
+    public class PlayerLoadingStrategy : ILoadingStrategy<IFile> {
+        public List<IFile> Load() {
+            throw new NotImplementedException();
+        }
+    }
+}
