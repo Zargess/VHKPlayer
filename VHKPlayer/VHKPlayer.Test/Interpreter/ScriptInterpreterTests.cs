4e788babf502760dc4d23227264e86c6e298e73a
Marcus Haagh <haagh9@hotmail.com>
2015-11-03 19:05:40 +0100

    ScriptInterpreter
    Can evaluate folder and type selector

---
+++ VHKPlayer/VHKPlayer.Test/Interpreter/ScriptInterpreterTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VHKPlayer.Interpreter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VHKPlayer.Models;
+using VHKPlayer.Test;
+using VHKPlayer.Queries.GetFolderByRelativePath;
+using VHKPlayer.Queries.Interfaces;
+using NSubstitute;
+using Ploeh.AutoFixture;
+using Autofac;
+using VHKPlayer.Interpreter.Interfaces;
+
+namespace VHKPlayer.Interpreter.Tests
+{
+    [TestClass()]
+    public class ScriptInterpreterTests : TestBase
+    {
+        [TestMethod]
+        public void TestEvaluateTypeSelector()
+        {
+            var script = "(type FileNode)";
+            var container = CreateContainer();
+            var interpreter = container.Resolve<IScriptInterpreter>();
+
+            Assert.IsTrue(interpreter.Evaluate(script, new FileNode()));
+            Assert.IsFalse(interpreter.Evaluate(script, new FolderNode(null)));
+        }
+
+        [TestMethod()]
+        public void TestEvaluateFolderSelector()
+        {
+            var script = "(folder \"root\\blandet\")";
+            var file = new FileNode()
+            {
+                FullPath = fixture.Create<string>()
+            };
+            var playablefile1 = new PlayableFile()
+            {
+                File = file
+            };
+            var playablefile2 = new PlayableFile()
+            {
+                File = new FileNode()
+                {
+                    FullPath = fixture.Create<string>()
+                }
+            };
+
+            var container = CreateContainer(c =>
+            {
+                c.RegisterFake<IQueryHandler<GetFolderByRelativePathQuery, FolderNode>>()
+                    .Handle(Arg.Any<GetFolderByRelativePathQuery>())
+                    .Returns(
+                        new FolderNode(null)
+                        {
+                            Content = new List<FileNode>()
+                            {
+                                file
+                            }
+                        });
+            });
+
+            var interpreter = container.Resolve<IScriptInterpreter>();
+
+            Assert.IsTrue(interpreter.Evaluate(script, playablefile1));
+            Assert.IsFalse(interpreter.Evaluate(script, playablefile2));
+        }
+    }
+}
--- VHKPlayer/VHKPlayer/Interpreter/ScriptInterpreter.cs
+++ VHKPlayer/VHKPlayer/Interpreter/ScriptInterpreter.cs
@@ -7,6 +7,7 @@ using VHKPlayer.Infrastructure;
 using VHKPlayer.Interpreter.Interfaces;
 using VHKPlayer.Models;
 using VHKPlayer.Queries.GetFolderByPathSubscript;
+using VHKPlayer.Queries.GetFolderByRelativePath;
 using VHKPlayer.Queries.Interfaces;
 using VHKPlayer.Utility.LoadingStrategy.Interfaces;
 using VHKPlayer.Utility.LoadingStrategy.PlayListLoading;
@@ -51,12 +52,42 @@ namespace VHKPlayer.Interpreter
 
         private bool HandleTypeScript(string script, object input)
         {
-            throw new NotImplementedException();
+            var parameter = script.Replace(")", "").Split(' ');
+            if (parameter.Length != 1 && !Regex.IsMatch(parameter[0], "[a-zA-z][a-zA-Z0-9]*"))
+            {
+                throw new SyntaxErrorException("Type must not be seperated by space and should not allow special characters or allow starting with number.\n" + script);
+            }
+
+            try
+            {
+                var t = Type.GetType("VHKPlayer.Models." + parameter[1]);
+                var temp = t == input.GetType();
+                return t == input.GetType();
+            }
+            catch (TypeLoadException ex)
+            {
+                Console.WriteLine("Type {0} was not recognised as a type.\n{1}", parameter[1], ex.StackTrace);
+            }
+
+            return false;
         }
 
         private bool HandleFolderScript(string script, object input)
         {
-            throw new NotImplementedException();
+            var match = Regex.Match(script, "\".*?\"");
+            if (!match.Success) throw new SyntaxErrorException("A folder selector must have the path in quotes!\n" + script);
+
+            var playablefile = input as PlayableFile;
+            if (playablefile == null) return false; // Not a playable file!
+
+            var relativePath = match.Value.Replace("\"", "");
+
+            var folder = processor.Process(new GetFolderByRelativePathQuery()
+            {
+                RelativePath = relativePath
+            });
+
+            return folder.Contains(playablefile.File);
         }
     }
 }
